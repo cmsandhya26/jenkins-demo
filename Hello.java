d0ccbc9f2f1a588df9d06b265d6836bf663e7973
cmsandhya26 <sandhya.cm2023@vitstudent.ac.in>
2026-02-08 18:05:46 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
+public class Hello {      //this is java code!!!
     public static void main(String[] args) {
-        System.out.println("Mini CI Project Success");
+        System.out.println("Mini CI Project Success!!");
     }
 }
